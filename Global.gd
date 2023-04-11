97329c5b5c9ef0807d5692157294ca3df2a4a3ad
Lyrune <122567031+Lyrune@users.noreply.github.com>
2023-04-11 13:08:55 -0400

    update 1
    oh wow making games is hard all of a sudden

--- Global.gd
+++ Global.gd
@@ -1,5 +1,19 @@
 extends Node
 
-func _unhandled_input(_event):
-	if Input.is_action_pressed("menu"):
-		get_tree().quit()
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+
+func _unhandled_input(event):
+	if event.is_action_pressed("menu"):
+		var Pause_Menu = get_node_or_null("/root/Game/Menu")
+		if Pause_Menu == null:
+			get_tree().quit()
+		else:
+			if get_tree().paused == true:
+				Pause_Menu.hide()
+				get_tree().paused = false
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			else:
+				Pause_Menu.show()
+				get_tree().paused = true
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
---
+++ UI/Menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _physics_process(delta):
+	if Input.is_action_pressed("menu"):
+		pass
+
+func _on_Restart_pressed():
+	get_tree().change_scene("res://Maze/Maze.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+
